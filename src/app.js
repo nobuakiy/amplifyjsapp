829f9cacc56a9e19812277dbdf4aa9f2608a9126
Nobuaki Yamashita <nobuakiy09@gmail.com>
2022-07-02 14:57:05 +0900

    fix and format

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import { Amplify, API, graphqlOperation } from "aws-amplify";
 
 import awsconfig from "./aws-exports";
 import { createTodo } from "./graphql/mutations";
+import { listTodos } from "./graphql/queries";
+import { onCreateTodo } from "./graphql/subscriptions";
 
 Amplify.configure(awsconfig);
 
@@ -14,11 +16,30 @@ async function createNewTodo() {
   return await API.graphql(graphqlOperation(createTodo, { input: todo }));
 }
 
+async function getData() {
+  API.graphql(graphqlOperation(listTodos)).then((evt) => {
+    evt.data.listTodos.items.map((todo, i) => {
+      QueryResult.innerHTML += `<p>${todo.name} - ${todo.description}</p>`;
+    });
+  });
+}
+
 const MutationButton = document.getElementById("MutationEventButton");
 const MutationResult = document.getElementById("MutationResult");
+const QueryResult = document.getElementById("QueryResult");
+const SubscriptionResult = document.getElementById("SubscriptionResult");
 
 MutationButton.addEventListener("click", (evt) => {
   createNewTodo().then((evt) => {
     MutationResult.innerHTML += `<p>${evt.data.createTodo.name} - ${evt.data.createTodo.description}</p>`;
   });
 });
+
+API.graphql(graphqlOperation(onCreateTodo)).subscribe({
+  next: (evt) => {
+    const todo = evt.value.data.onCreateTodo;
+    SubscriptionResult.innerHTML += `<p>${todo.name} - ${todo.description}</p>`;
+  },
+});
+
+getData();
